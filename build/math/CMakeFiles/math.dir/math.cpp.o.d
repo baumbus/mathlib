0ccbfe4593876493b9af4f676982d558e83c3f91
baumbus <88167473+baumbus@users.noreply.github.com>
2022-12-19 14:49:11 +0100

    -

--- build/math/CMakeFiles/math.dir/math.cpp.o.d
+++
@@ -1,2 +0,0 @@
-math/CMakeFiles/math.dir/math.cpp.o: \
- /home/baumbus/repos/mathlib/math/math.cpp /usr/include/stdc-predef.h
--- build/src/CMakeFiles/math.dir/math.cpp.o.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/math.dir/math.cpp.o: \
- /home/baumbus/repos/mathlib/src/math.cpp /usr/include/stdc-predef.h
